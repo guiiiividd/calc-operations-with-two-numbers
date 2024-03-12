0ff9f7250a185bd21141eca1c254742742aa6064
guiiiividd <guiiiividd@gmail.com>
2024-03-12 15:09:44 -0300

    Criação da Estrutura HTML

---
+++ src/app/form-calc/form-calc.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-calc',
+  templateUrl: './form-calc.component.html',
+  styleUrl: './form-calc.component.css'
+})
+export class FormCalcComponent {
+
+  res: string = "O resultado da operação de divisão entre 10 e 2 é: 5";
+}
